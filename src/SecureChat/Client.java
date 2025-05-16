1fdb8b8fe13b02b5c32d52eab03a9944c167fad5
edisonndou02 <edison.ndou@student.uni-pr.edu>
2025-05-16 10:47:44 +0200

    Update Client.java

--- src/SecureChat/Client.java
+++ src/SecureChat/Client.java
@@ -1,4 +1,9 @@
 public class Client {
     public static void main(String[] args)  {
+        Socket s = new Socket("localhost", 5000);
+        DataInputStream  in  = new DataInputStream(s.getInputStream());
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        KeyPair myKP = CryptoUtils.genDH();
     }
 }
