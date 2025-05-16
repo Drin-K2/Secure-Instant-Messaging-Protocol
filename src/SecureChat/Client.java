35b6c5f5ef81218544b552df1929a8c10141ee87
edisonndou02 <edison.ndou@student.uni-pr.edu>
2025-05-16 10:54:13 +0200

    Handling Server Fail

--- src/SecureChat/Client.java
+++ src/SecureChat/Client.java
@@ -1,28 +1,33 @@
 public class Client {
     public static void main(String[] args)  {
-        Socket s = new Socket("localhost", 5000);
-        DataInputStream  in  = new DataInputStream(s.getInputStream());
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-        KeyPair myKP = CryptoUtils.genDH();
-        int len = in.readInt();
-        byte[] srvPubEnc = new byte[len];
-        in.readFully(srvPubEnc);
-        PublicKey srvPub = KeyFactory.getInstance("DH")
-                .generatePublic(new X509EncodedKeySpec(srvPubEnc));
-
-        out.writeInt(myKP.getPublic().getEncoded().length);
-        out.write(myKP.getPublic().getEncoded());
-
-        SecretKey aes = CryptoUtils.agreeAES(myKP, srvPub);
-        System.out.println("Shared AES gati!");
-
-        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            System.out.print("Client> ");
-            String msg = stdin.readLine();
-            out.writeUTF(CryptoUtils.encrypt(aes, msg));
-            String resp = in.readUTF();
-            System.out.println("Server: " + CryptoUtils.decrypt(aes, resp));
+        try {
+            Socket s = new Socket("localhost", 5000);
+            DataInputStream  in  = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+    
+            KeyPair myKP = CryptoUtils.genDH();
+            int len = in.readInt();
+            byte[] srvPubEnc = new byte[len];
+            in.readFully(srvPubEnc);
+            PublicKey srvPub = KeyFactory.getInstance("DH")
+                    .generatePublic(new X509EncodedKeySpec(srvPubEnc));
+    
+            out.writeInt(myKP.getPublic().getEncoded().length);
+            out.write(myKP.getPublic().getEncoded());
+    
+            SecretKey aes = CryptoUtils.agreeAES(myKP, srvPub);
+            System.out.println("Shared AES gati!");
+    
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                System.out.print("Client> ");
+                String msg = stdin.readLine();
+                out.writeUTF(CryptoUtils.encrypt(aes, msg));
+                String resp = in.readUTF();
+                System.out.println("Server: " + CryptoUtils.decrypt(aes, resp));
+            }
+        } catch (Exception e) {
+            System.out.println("Serveri u shkëput nga lidhja.");
+        }
     }
 }
