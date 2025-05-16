d73fb5c12637e3d2ef374ad180f9622a73b2baef
DuaZogu <dua.zogu@student.uni-pr.edu>
2025-05-16 11:18:20 +0200

    Create Server.java
    Krijimi i serverit qe eshte i lidhur me portin 5000 dhe pranon nje lidhje nga klienti.
    Perdoret algoritmi Diffie-Helman per shkembimin e celesave publik mes serverit dhe klientit.
    Gjenerohet nje celes i perbashket AES per komunikim te enkriptuar .
    Mesazhet qe dergohen dhe pranohen enkriptohen/dekriptohen duke perdorur celes AES.
    Komunikimi realizohet ne menyre te sigurt dhe ne te dy drejtimet duke perdorur DataInputStream dhe DataOutputStream

---
+++ src/SecureChat/Server.java
@@ -0,0 +1,35 @@
+public class Server {
+    public static void main(String[] args) throws Exception {
+        try {
+        ServerSocket ss = new ServerSocket(5000);
+        System.out.println("Server gati...");
+        Socket s = ss.accept();
+        DataInputStream  in  = new DataInputStream(s.getInputStream());
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+
+        KeyPair myKP = CryptoUtils.genDH();
+        out.writeInt(myKP.getPublic().getEncoded().length);
+        out.write(myKP.getPublic().getEncoded());
+
+        int len = in.readInt();
+        byte[] cliPubEnc = new byte[len];
+        in.readFully(cliPubEnc);
+        PublicKey cliPub = KeyFactory.getInstance("DH")
+                .generatePublic(new X509EncodedKeySpec(cliPubEnc));
+
+        SecretKey aes = CryptoUtils.agreeAES(myKP, cliPub);
+        System.out.println("Shared AES gati!");
+
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String enc = in.readUTF();
+            System.out.println("Klienti: " + CryptoUtils.decrypt(aes, enc));
+            System.out.print("Server> ");
+            String reply = stdin.readLine();
+            out.writeUTF(CryptoUtils.encrypt(aes, reply));
+        }} catch (Exception e) {
+            System.out.println("Klienti u shkëput nga lidhja.");
+        }
+    }
+}
